6a871bf4fd5c15469678fe3f33c8ed1a5811a1aa
Prince Goswami <prince@mrprincegoswami.in>
2024-08-03 00:28:53 +0530

    social icons changed

--- frontend_react/src/components/SocialMedia/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia/SocialMedia.jsx
@@ -1,22 +1,28 @@
-import React from 'react'
-import {BsTwitter, BsInstagram} from "react-icons/bs";
-import {FaFacebookF} from "react-icons/fa";
-import './SocialMedia.css'
+import React from "react";
+import { SiLeetcode } from "react-icons/si";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import "./SocialMedia.css";
 
 const SocialMedia = () => {
   return (
     <div className="app__social">
-        <div className="social__container " >
-            <BsTwitter />
-        </div>
-        <div className='social__container'>
-            <FaFacebookF />
-        </div>
-        <div className='social__container'>
-            <BsInstagram />
-        </div>
+      <div className="social__container ">
+        <a href="https://www.linkedin.com/in/mrprincegoswami" target="_blank">
+          <FaLinkedin />
+        </a>
+      </div>
+      <div className="social__container">
+        <a href="https://leetcode.com/u/MrPrinceGoswami/" target="_blank">
+          <SiLeetcode />
+        </a>
+      </div>
+      <div className="social__container">
+        <a href="https://github.com/mr2space" target="_blank">
+          <FaGithub />
+        </a>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default SocialMedia;
